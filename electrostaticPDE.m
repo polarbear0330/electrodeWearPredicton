0303c8ad9ce1d1990331bd2c275eae9bf455b3dd
taoxumuye <tfy.hi@163.com>
2018-05-31 19:26:06 +0800

    pde programmatically instead pdeToolBox GUI

---
+++ electrostaticPDE.m
@@ -0,0 +1,67 @@
+function [ u,E,pos,points_E,EE,max_absE,point_max,maxE ] = electrostaticPDE( dl )
+%ELECTROSTATICPDE 此处显示有关此函数的摘要
+%   此处显示详细说明
+
+figure(1);
+pdegplot(dl,'EdgeLabels','on','FaceLabels','on')
+xlim([-1.5,1.5])
+axis equal
+
+
+% model = createpde;
+% geometryFromEdges(model,dl);
+% figure(1);
+% pdegplot(model,'EdgeLabels','on');
+
+
+
+figure(2);
+% [p,e,t] = initmesh(dl);%默认1.3 
+[p,e,t] = initmesh(dl,'Hgrad',1.9); 
+subplot(2,2,1), pdemesh(p,e,t) 
+[p,e,t] = refinemesh(dl,p,e,t); 
+subplot(2,2,2), pdemesh(p,e,t) 
+% [p,e,t] = refinemesh(dl,p,e,t); 
+% subplot(2,2,3), pdemesh(p,e,t) 
+% [p,e,t] = refinemesh(dl,p,e,t); 
+% subplot(2,2,4), pdemesh(p,e,t) 
+
+figure(3);
+b = allzerobc(dl);%boundary matrix, MODEL!!!
+% applyBoundaryCondition(model,'dirichlet','edge',1:model.Geometry.NumEdges,'u',0);
+applyBoundaryCondition(b,'dirichlet','edge',2,'u',150);
+applyBoundaryCondition(b,'dirichlet','edge',7,'u',150);
+applyBoundaryCondition(b,'dirichlet','edge',8,'u',150);
+u=assempde(b,p,e,t,1,0,0); 
+pdeplot(p,e,t,'XYData',u)
+
+
+
+
+%------------------E-------------------------------------------------------
+point = p;
+%pdetool计算三角形中心处梯度——场强
+[Ex,Ey]=pdegrad(point,t,u);
+E = [Ex;Ey];
+EE = sqrt(Ex.^2 + Ey.^2);
+[max_absE,pos] = max(EE,[],2);%dim=2：每一行的最大值
+
+% Triangle point indices
+it1 = t(1,:);
+it2 = t(2,:);
+it3 = t(3,:);
+% Find centroids of triangles
+xpts = (point(1,it1) + point(1,it2) + point(1,it3))/3;
+ypts = (point(2,it1) + point(2,it2) + point(2,it3))/3;
+points_E = [xpts;ypts];
+
+% figure(4);
+% pdeplot(points_E,e,t,'XYData',EE)
+
+%向量E起点
+point_max = points_E(:,pos);
+%向量E
+maxE = E(:,pos);
+
+end
+
--- main.m
+++ main.m
@@ -32,14 +32,34 @@ spark=zeros(sparkDist*gridRatio,30000*gridRatio);% 放电间隙sparkDist至少3个网格
 matrix=[h1; t; spark; w; h1];
 toc
 
+geom_dl = [2,2,2,2,2,2,2,2,2,2,2,2;
+    100, -30, 150, -150, 100,-100, 30, -30, -150, 100, 150, -30;
+    -100, 30, 150, -150, 100,-100, 30, -30, -100, 150,  30,-150;
+    50,   51,   0,  300,   0,  50, 51, 300,    0,   0, 300, 300;
+    50,   51, 300,    0,  50,   0,300,  51,    0,   0, 300, 300;
+    0,     0,   1,    1,   0,   0,  0,   0,    1,   1,   1,   1;
+    1,     1,   0,    0,   1,   1,  1,   1,    0,   0,   0,   0];
+
+
+
 %--------------------------------------------------------------------------
+
+
 % 边界跟踪（输入：代表正负电极形状的矩阵；输出：正负电极边界点行列）
 disp('boundary trace:');
-tic,[wm,wn,tm,tn]=boundaryTrace_Main(matrix);toc
+tic,[wm,wn,tm,tn] = boundaryTrace_Main(matrix);toc
+
+% 1.矩阵点连接成边，相同斜率的边是同一个边
+% 2.构建 geom 矩阵
+%（输入：boundary points；输出：连接好的edge组成的geom_dl矩阵）
+%（注：二维图形的pde_geom矩阵，暂时不会三维矩阵的构造）
+disp('generate pde Geometry:');
+% tic,[geom_dl] = pdeEdgeGeom(boundaryPoints);toc
 
 % 电场计算（输入：电极形状，边界条件；输出：~,矢量E，E的起点坐标，E的大小）
 disp('calculate E');
-tic,[~,~,~,~,~,maxAbsE,maxPoint,maxE] = wholeE;toc
+tic,[~,~,~,~,~,maxAbsE,maxPoint,maxE] = electrostaticPDE(geom_dl);toc
+% tic,[~,~,~,~,~,maxAbsE,maxPoint,maxE] = wholeE;toc
 
 % 获取放电点对（输入：边界点行列，E，grid边长，左上顶点实际坐标；输出：放点电行列）
 disp('spark point:');
@@ -47,9 +67,9 @@ tic,
 origin_left_up=[...
     -size(matrix,2)/2, ...
     size(matrix,1)-size(h1,1)...
-    ]*grid/1000/10; % 仍需修改，尤其是横坐标，/10是转化成了厘米
-[sparkpoint_tool] = sparkPoint(tm,tn,maxPoint',maxE,maxAbsE,grid/1000/10,origin_left_up);% 仍需修改，尤其是横坐标，/10是转化成了厘米
-[sparkpoint_workp] = sparkPoint(wm,wn,maxPoint',maxE,maxAbsE,grid/1000/10,origin_left_up);
+    ]*grid/1000*10; % 仍需修改，尤其是横坐标，/10是转化成了厘米
+[sparkpoint_tool] = sparkPoint(tm,tn,maxPoint',maxE,maxAbsE,grid/1000*10,origin_left_up);% 仍需修改，尤其是横坐标，/10是转化成了厘米
+[sparkpoint_workp] = sparkPoint(wm,wn,maxPoint',maxE,maxAbsE,grid/1000*10,origin_left_up);
 toc
 
 % 蚀除（输入：完整矩阵，两放点电，两蚀坑半径；输出：完整矩阵）
